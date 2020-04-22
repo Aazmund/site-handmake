ea2b1048c37f9cd1062e9119694cd40b775023a2
SchooKi <59267666+SchooKi@users.noreply.github.com>
2020-04-22 13:53:54 +0300

    fix slider

--- js/sl.js
+++ js/sl.js
@@ -1,1 +1,11 @@
-$('.sl').slick();
+$(".sl").slick({
+  dots: true,
+  prevArrow:
+    "<img class='a-left control-c prev slick-prev' src='../image/arrowl.png'>",
+  nextArrow:
+    "<img class='a-right control-c next slick-next' src='../image/arrowr.png'>",
+  centerMode: true,
+  slidesToShow: 1,
+  adaptiveHeight: true,
+  variableWidht: true
+});
